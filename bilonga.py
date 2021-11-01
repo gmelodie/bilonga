36e1d07c919d4246d99c0c439b83f17d4ca0ab1f
Gabriel Cruz <gabrieldemelocruz@gmail.com>
2021-10-31 21:24:21 -0300

    Add default handler

--- bilonga.py
+++ bilonga.py
@@ -1,11 +1,14 @@
 import socket
 import threading
 
-def process_incomming_connection(connection, out_sock):
+def default_handler(data):
+    return data
+
+def process_incomming_connection(connection, handler_func, out_sock):
     with connection:
         data = connection.recv(1024)
         while data:
-            out_sock.sendall(data)
+            out_sock.sendall(handler_func(data.decode()).encode())
             data = connection.recv(1024)
 
 
@@ -21,14 +24,13 @@ def create_tunnel(in_port, out_port):
         while True:
             in_conn, in_addr = in_sock.accept()
             print(f"Incomming connection from {in_addr}")
-            threading.Thread(target=process_incomming_connection, args=(in_conn, out_sock)).start()
-
+            # TODO: change default_handler to decorator
+            threading.Thread(target=process_incomming_connection, args=(in_conn, default_handler, out_sock)).start()
 
 def bilonga(in_out_ports=[(80,8080)]):
     for in_port, out_port in in_out_ports:
         threading.Thread(target=create_tunnel, args=(in_port, out_port)).start()
         print(f'New tunnel {in_port} -> {out_port}')
-
 
 if __name__ == '__main__':
     bilonga([(85,86), (87, 88)])
